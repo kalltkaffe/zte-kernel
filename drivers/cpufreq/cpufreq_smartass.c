b909ecddd8c9cbadee061297a90eb72161b2dbe9
kallt_kaffe <kallt_kaffe@apedroid.com>
2011-07-11 09:43:03 +0200

    Tweak Smartass to allow tunein radio to run better with screen off.

--- drivers/cpufreq/cpufreq_smartass.c
+++ drivers/cpufreq/cpufreq_smartass.c
@@ -95,7 +95,7 @@ static unsigned int up_min_freq;
  * to minimize wakeup issues.
  * Set sleep_max_freq=0 to disable this behavior.
  */
-#define DEFAULT_SLEEP_MAX_FREQ 245760
+#define DEFAULT_SLEEP_MAX_FREQ 320000
 static unsigned int sleep_max_freq;
 
 /*
